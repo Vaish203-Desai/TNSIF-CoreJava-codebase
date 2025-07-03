e8b1485b476d2889db94d60dc8b13312eee72061
Vaishnavi Desai <desaivaishnavi04@gmail.com>
2025-07-03 23:46:29 +0530

    updated

---
+++ src/com/Encapsulation/EncapsulationDemo.java
@@ -0,0 +1,17 @@
+package com.Encapsulation;
+
+public class EncapsulationDemo {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		EncapsulationOOP obj=new EncapsulationOOP();
+		
+		obj.setName("Vaishnavi");
+		obj.setAge(20);
+		obj.setSerailNum(13133);
+		
+		System.out.println(obj);
+	}
+
+}
